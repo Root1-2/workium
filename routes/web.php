caec350e7200ba7517d711bd833f2c4b5cb777df
root12 <abdullahalmasrur8@gmail.com>
2024-10-16 20:55:02 +0600

    create views

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs', function () {
-    return "<h1>Available Jobs<h1>";
+    return view('jobs.index');
 })->name("jobs");
 
-Route::get("/test", function() {
-    return response()->json(["name" => "John Doe"])->cookie("name", "Brad");
-});
-
-Route::get("/error", function() {
-    return response("Not Found", 404);
-});
-
-Route::get("/download", function() {
-    return response()->download(public_path("favicon.ico"));
-});
-
-Route::get("/read-cookie", function(Request $request) {
-    $cookieValue = $request->cookie("name");
-    return response()->json(["cookie" => $cookieValue]);
-});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name("jobs.create");
