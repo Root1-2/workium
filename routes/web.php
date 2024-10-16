a9d968f14099b89670859f9c4446365e97da0895
root12 <abdullahalmasrur8@gmail.com>
2024-10-16 20:49:09 +0600

    helper functions for response

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,19 @@ Route::get('/jobs', function () {
     return "<h1>Available Jobs<h1>";
 })->name("jobs");
 
-Route::get("/test", function (Request $request) {
-    return [
-        "method" => $request->method(),
-        "url" => $request->url(),
-        "path" => $request->path(),
-        "fullUrl" => $request->fullUrl(),
-        "ip" => $request->ip(),
-        "userAgent" => $request->userAgent(),
-        "header" => $request->header()
-    ];
+Route::get("/test", function() {
+    return response()->json(["name" => "John Doe"])->cookie("name", "Brad");
 });
 
-Route::get("/users", function(Request $request) {
-    return $request->input("name");
+Route::get("/error", function() {
+    return response("Not Found", 404);
+});
+
+Route::get("/download", function() {
+    return response()->download(public_path("favicon.ico"));
+});
+
+Route::get("/read-cookie", function(Request $request) {
+    $cookieValue = $request->cookie("name");
+    return response()->json(["cookie" => $cookieValue]);
 });
