3fde6574f40ab2b8e30e849e79b8df6e26c5cc0f
root12 <abdullahalmasrur8@gmail.com>
2024-10-16 21:00:40 +0600

    passing data to views

--- resources/views/jobs/index.php
+++ resources/views/jobs/index.php
@@ -6,12 +6,11 @@
     <title>Job Listings</title>
 </head>
 <body>
-    <h1><?php echo "Web Developer" ?></h1>
+    <h1><?php echo $title ?></h1>
     <ul>
-        <li>Web Developers</li>
-        <li>Database Admin</li>
-        <li>Software Engineer</li>
-        <li>System Analyst</li>
+        <?php foreach($jobs as $job) : ?>
+            <li><?php echo htmlspecialchars($job, ENT_QUOTES, "UTF-8"); ?></li>
+        <?php endforeach ?>
     </ul>
 </body>
 </html>
