1d483f3b71f05b06cf6c82d3069ace08edb31eb6
root12 <abdullahalmasrur8@gmail.com>
2024-10-17 23:44:28 +0600

    tailwind init

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=`, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>{{ $title ?? 'Workium | Find/List Jobs' }}</title>
 </head>
 
