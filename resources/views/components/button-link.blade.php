d90d123b0ebe3b045c18c2466c797319a0977b4b
root12 <abdullahalmasrur8@gmail.com>
2024-10-19 19:41:18 +0600

    button link

---
+++ resources/views/components/button-link.blade.php
@@ -0,0 +1,15 @@
+@props([
+    'url' => '/',
+    'icon' => null,
+    'color' => 'bg-yellow-500',
+    'hover' => 'hover:bg-yellow-600',
+    'textClass' => 'text-black',
+])
+
+<a href="{{ $url }}"
+    class="{{ $color }} {{ $hover }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300">
+    @if ($icon)
+        <i class="fa fa-{{ $icon }}"></i>
+    @endif
+    {{ $slot }}
+</a>
