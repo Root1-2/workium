27012585deba26da46cc850319ca406be55ac654
root12 <abdullahalmasrur8@gmail.com>
2024-10-20 01:47:50 +0600

    input validation, errors, old attr

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,8 +3,19 @@
     <h2>Create New Job</h2>
     <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="title">
-        <input type="text" name="description" placeholder="description">
-        <button type="submit">Submit</button>
+        <div class="my-5">
+            <input type="text" name="title" placeholder="title" value={{ old('title') }}>
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="my-5">
+            <input type="text" name="description" placeholder="description" value={{ old('description') }}>
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+        <button class="bg-green-200 rounded-lg px-4 py-2" type="submit">Submit</button>
     </form>
 </x-layout>
