6307dc2932e7195e31341dd23d74914db5ab5146
root12 <abdullahalmasrur8@gmail.com>
2024-10-19 19:49:52 +0600

    mobile navBar

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -7,7 +7,7 @@
 ])
 
 <a href="{{ $url }}"
-    class="{{ $color }} {{ $hover }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300">
+    class="{{ $color }} {{ $hover }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 block md:inline">
     @if ($icon)
         <i class="fa fa-{{ $icon }}"></i>
     @endif
