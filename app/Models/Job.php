e6b26773856ee4db2e6579fcbd31948860ea7234
root12 <abdullahalmasrur8@gmail.com>
2024-10-20 02:01:43 +0600

    job schema migration update

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,7 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
     protected $table = "job_listings";
 
-    protected $fillable = ["title", "description"];
+    protected $fillable = [
+        "title",
+        "description",
+        "salary",
+        "tags",
+        "job_type",
+        "remote",
+        "requirements",
+        "benefits",
+        "address",
+        "city",
+        "state",
+        "zipcode",
+        "contact_email",
+        "contact_phone",
+        "company_name",
+        "company_description",
+        "company_logo",
+        "company_website",
+    ];
 }
