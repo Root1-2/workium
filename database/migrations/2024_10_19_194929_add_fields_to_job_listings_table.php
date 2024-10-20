983898aa9d700bf0d7e8631eec80296732ea9989
root12 <abdullahalmasrur8@gmail.com>
2024-10-20 20:45:38 +0600

    table relationship init

--- database/migrations/2024_10_19_194929_add_fields_to_job_listings_table.php
+++ database/migrations/2024_10_19_194929_add_fields_to_job_listings_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         DB::table("job_listings")->truncate();
 
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->unsignedBigInteger("user_id")->after("id");
+
             $table->integer("salary");
             $table->string("tags")->nullable();
             $table->enum("job_type", ["Full-Time", "Part-Time", "Contract", "Remote", "Internship", "Temporary"])->default("Full-Time");
@@ -33,6 +35,9 @@ return new class extends Migration
             $table->text("company_description");
             $table->string("website")->nullable();
             $table->string("company_logo")->nullable();
+
+            // Add user foreign key contraint
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
         });
     }
 
@@ -42,6 +47,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropForeign(["user_id"]);
+            $table->dropColumn("user_id");
+
             $table->dropColumn([
                 "salary",
                 "tags",
