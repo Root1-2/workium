1a38adeb549dea0f6c46f546b50ddd0cc772f3e3
root12 <abdullahalmasrur8@gmail.com>
2024-10-23 03:36:01 +0600

    login done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,11 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login(): View
     {
         return view("auth.login");
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            "email" => "required|string|email|max:100",
+            "password" => "required|string"
+        ]);
+
+        // Attempt to Authenticate User
+        if (Auth::attempt($credentials)) {
+            // Regenerate the session to prevent to fixation attack
+            $request->session()->regenerate();
+            return redirect()->intended(route("home"))->with('Success', 'You are now Logged In');
+        }
+
+        // If auth fails, redirect back with error
+        return back()->withErrors([
+            "email" => "The provided credentials do not match our records",
+        ])->onlyInput("email");
     }
 }
