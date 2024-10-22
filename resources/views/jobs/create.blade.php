f2c0e9f51c79474cc27ba9610a736adf4433d76c
root12 <abdullahalmasrur8@gmail.com>
2024-10-23 02:03:51 +0600

    update job listing

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Create Job</x-slot>
     <div class="bg-white mx-auto p-8 rounded-lg shadow-md w-full md:max-w-3xl">
         <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
-        <form method="POST" action="/jobs" enctype="multipart/form-data">
+        <form method="POST" action="{{ route('jobs.store') }}" enctype="multipart/form-data">
             @csrf
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
@@ -50,12 +50,6 @@
 
             <x-inputs.text id="company_name" name="company_name" label="Company Name"
                 placeholder="Enter Company Name" />
-
-            <div class="mb-4">
-                <label class="block text-gray-700" for="company_description">Company Description</label>
-                <textarea id="company_description" name="company_description" class="w-full px-4 py-2 border rounded focus:outline-none"
-                    placeholder="Company Description"></textarea>
-            </div>
 
             <x-inputs.text-area id="company_description" name="company_description" label="Company Description"
                 placeholder="Enter Company Description" />
