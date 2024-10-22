5561c636577a094c3d30a77197242a50360ebe48
root12 <abdullahalmasrur8@gmail.com>
2024-10-23 03:26:15 +0600

    registration done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(): View
     {
         return view("auth.register");
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validatedData = $request->validate([
+            "name" => "required|string|max:100",
+            "email" => "required|string|email|max:100|unique:users",
+            "password" => "required|string|min:8|confirmed",
+        ]);
+
+        // Hash Password
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        // Create User
+        $user = User::create($validatedData);
+
+        return redirect()->route("login")->with("success", "You are registerd to the website");
     }
 }
