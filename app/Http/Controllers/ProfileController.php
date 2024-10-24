54a190624d912562a9b8090a05733ee5292d400f
root12 <abdullahalmasrur8@gmail.com>
2024-10-25 02:29:24 +0600

    avatar upload

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -17,11 +18,26 @@ class ProfileController extends Controller
         $validatedData = $request->validate([
             "name" => "required|string",
             "email" => "required|string|email",
+            "avatar" => "nullable|image|mimes:jpeg,jpg,png,gif,ico|max:2048"
         ]);
 
-        // dd($user);
+        // Get User name and email
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        // Handle Avatar Upload
+        if ($request->hasFile('avatar')) {
+            // Delete Old Avatar if exist
+            if ($user->avatar) {
+                unlink(storage_path('app/public/' . $user->avatar));
+            }
+
+            // Store new avatar
+            $avatarPath = $request->file("avatar")->store('avatars', 'public');
+            $user->avatar = $avatarPath;
+        }
         // Update User Info
-        $user->update($validatedData);
+        $user->save();
 
         return redirect()->route("dashboard")->with("Success", "Profile Info Updated");
     }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,12 +4,21 @@
         <div class="bg-white p-8 rounded-lg shadow-md w-full">
             <h3 class="text-3xl text-center font-bold mb-4">Profile Info</h3>
 
+            @if ($user->avatar)
+                <div class="flex justify-center mt-2">
+                    <img class="w-32 h-32 object-cover rounded-full" src="{{ asset('storage/' . $user->avatar) }}"
+                        alt="{{ $user->name }}">
+                </div>
+            @endif
+
             <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
 
                 <x-inputs.text id="name" name="name" label="Name" value="{{ $user->name }}" />
                 <x-inputs.text id="email" name="email" label="Email Address" value="{{ $user->email }}" />
+                <input type="file" class="mb-3 border w-full px-4 py-2 rounded" id="avatar" name="avatar"
+                    label="Upload Avatar" />
 
                 <button type="submit"
                     class="w-full bg-green-500 text-white hover:bg-green-700 px-4 py-2 border rounded focus:outline-none">Save</button>
