c8b22e423beaf4d7c3621881d0451886ebeb161a
root12 <abdullahalmasrur8@gmail.com>
2024-10-26 19:00:44 +0600

    job search component, routes added

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::post("/logout", [LoginController::class, "logout"])->name("logout");
 
 Route::get('/', [HomeController::class, "index"])->name("home");
 
-// Route::resource("jobs", JobController::class);
+Route::get("/jobs/search", [JobController::class, "search"])->name("jobs.search");
 Route::resource("jobs", JobController::class)->middleware("auth")->only(["create", "edit", "update", "destroy"]);
 Route::resource("jobs", JobController::class)->except(["create", "edit", "update", "destroy"]);
 
